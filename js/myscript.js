bfcc4cd4e859fac4882f03bf6fb8fa74aeac75cb
Eric Duenes <Eric.J.Duenes@kp.org>
2020-10-18 21:38:28 -0700

    completed service page.

---
+++ js/myscript.js
@@ -0,0 +1,43 @@
+function strategySelected() {
+  var strategyDiv = document.getElementById("strategyDiv");
+  var talentDiv = document.getElementById("talentDiv");
+  var leadershipDiv = document.getElementById("leadershipDiv");
+  var planningDiv = document.getElementById("planningDiv");
+  strategyDiv.style.display = "block";
+  talentDiv.style.display = "none";
+  leadershipDiv.style.display = "none";
+  planningDiv.style.display = "none";
+}
+
+function talentSelected() {
+  var strategyDiv = document.getElementById("strategyDiv");
+  var talentDiv = document.getElementById("talentDiv");
+  var leadershipDiv = document.getElementById("leadershipDiv");
+  var planningDiv = document.getElementById("planningDiv");
+  strategyDiv.style.display = "none";
+  talentDiv.style.display = "block";
+  leadershipDiv.style.display = "none";
+  planningDiv.style.display = "none";
+}
+
+function leadershipSelected() {
+  var strategyDiv = document.getElementById("strategyDiv");
+  var talentDiv = document.getElementById("talentDiv");
+  var leadershipDiv = document.getElementById("leadershipDiv");
+  var planningDiv = document.getElementById("planningDiv");
+  strategyDiv.style.display = "none";
+  talentDiv.style.display = "none";
+  leadershipDiv.style.display = "block";
+  planningDiv.style.display = "none";
+}
+
+function planningSelected() {
+  var strategyDiv = document.getElementById("strategyDiv");
+  var talentDiv = document.getElementById("talentDiv");
+  var leadershipDiv = document.getElementById("leadershipDiv");
+  var planningDiv = document.getElementById("planningDiv");
+  strategyDiv.style.display = "none";
+  talentDiv.style.display = "none";
+  leadershipDiv.style.display = "none";
+  planningDiv.style.display = "block";
+}
